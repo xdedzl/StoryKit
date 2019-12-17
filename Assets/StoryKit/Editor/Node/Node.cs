c2fad4474d621a4c46dac717c8e99e537cfaf51e
xdedzl <xdedzl@163.com>
2019-12-17 21:17:14 +0800

    样式表第一次尝试成功

---
+++ Assets/StoryKit/Editor/Node/Node.cs
@@ -0,0 +1,81 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace XFramewrok.StoryKit
+{
+    public class Node : VisualElement
+    {
+        private bool isMove;
+        // 用UXML
+        public class Fatory : UxmlFactory<Node> { }
+
+        public Node()
+        {
+            Image image1 = new Image()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture>("Assets/StoryKit/Res/basic.png"),
+                tintColor = Color.white,
+
+                transform =
+                {
+                    position = new Vector3(0,15,0),
+                }
+            };
+            image1.AddToClassList("connectPoint");
+
+            Image image2 = new Image()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture>("Assets/StoryKit/Res/basic.png"),
+                tintColor = Color.red,
+
+                transform =
+                {
+                    position = new Vector3(0,0,0),
+                }
+            };
+            image2.AddToClassList("mainBody");
+
+            image2.RegisterCallback<MouseMoveEvent>((a) =>
+            {
+                Debug.Log("MouseMove");
+                if (isMove)
+                    this.transform.position += (Vector3)a.mouseDelta;
+            });
+            image2.RegisterCallback<PointerDownEvent>((a) =>
+            {
+                Debug.Log("PointerDownEvent");
+                this.BringToFront();
+                isMove = true;
+            });
+            image2.RegisterCallback<PointerUpEvent>((a) =>
+            {
+                Debug.Log("PointerUpEvent");
+
+                isMove = false;
+            });
+            image2.RegisterCallback<PointerEnterEvent>((a) =>
+            {
+                Debug.Log("PointerEnterEvent");
+
+                isMove = false;
+            });
+
+            Image image3 = new Image()
+            {
+                image = AssetDatabase.LoadAssetAtPath<Texture>("Assets/StoryKit/Res/basic.png"),
+                tintColor = Color.white,
+
+                transform =
+                {
+                    position = new Vector3(0,15,0),
+                }
+            };
+            image3.AddToClassList("connectPoint");
+
+            Add(image1);
+            Add(image2);
+            Add(image3);
+        }
+    }
+}
---
+++ Assets/StoryKit/Editor/StoryKitWindow.cs
@@ -0,0 +1,123 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace XFramewrok.StoryKit
+{
+    public class StoryKitWindow : EditorWindow
+    {
+        [MenuItem("XFramework/StoryKit")]
+        public static void ShowExample()
+        {
+            StoryKitWindow window = GetWindow<StoryKitWindow>();
+            window.minSize = new Vector2(450, 514);
+            window.titleContent = new GUIContent("StoryKit");
+        }
+
+        private Vector2 offset;
+        private Vector2 drag;
+
+        private VisualElement m_NodeRoot;
+
+        private StyleSheet nodeStyle;
+        private void OnEnable()
+        {
+            nodeStyle = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/StoryKit/Editor/Node/Node.uss");
+
+            var root = rootVisualElement;
+            root.styleSheets.Add(AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/StoryKit/Editor/StoryKitWindow.uss"));
+            m_NodeRoot = new VisualElement
+            {
+                style =
+                {
+                }
+            };
+            root.Add(m_NodeRoot);
+
+            IMGUIContainer imgui = new IMGUIContainer(() =>
+            {
+                DrawGrid(20, 0.2f, Color.gray);
+                DrawGrid(100, 0.4f, Color.gray);
+
+                ProcessEvents(Event.current);
+            });
+            root.Add(imgui);
+        }
+
+        private void ProcessEvents(Event e)
+        {
+            drag = Vector2.zero;
+
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    if (e.button == 0)
+                    {
+                        //ClearConnectionSelection();
+                    }
+
+                    if (e.button == 1)
+                    {
+                        ProcessContextMenu(e.mousePosition);
+                    }
+                    break;
+
+                case EventType.MouseDrag:
+                    if (e.button == 0)
+                    {
+                        //OnDrag(e.delta);
+                    }
+                    break;
+            }
+        }
+
+        private void ProcessContextMenu(Vector2 mousePosition)
+        {
+            GenericMenu genericMenu = new GenericMenu();
+            genericMenu.AddItem(new GUIContent("Add node"), false, () =>
+            {
+                Node node = new Node
+                {
+                    transform =
+                    {
+                        position = mousePosition
+                    },
+                };
+                node.styleSheets.Add(nodeStyle);
+                rootVisualElement.Add(node);
+            });
+            genericMenu.ShowAsContext();
+        }
+
+        /// <summary>
+        /// 绘制面板网格
+        /// </summary>
+        /// <param name="gridSpacing">间隔</param>
+        /// <param name="gridOpacity">透明度</param>
+        /// <param name="gridColor">颜色</param>
+        private void DrawGrid(float gridSpacing, float gridOpacity, Color gridColor)
+        {
+            int widthDivs = Mathf.CeilToInt(position.width / gridSpacing);
+            int heightDivs = Mathf.CeilToInt(position.height / gridSpacing);
+
+            Handles.BeginGUI();
+            Handles.color = new Color(gridColor.r, gridColor.g, gridColor.b, gridOpacity);
+
+            offset += drag * 0.5f;
+            Vector3 newOffset = new Vector3(offset.x % gridSpacing, offset.y % gridSpacing, 0);
+
+            for (int i = 0; i < widthDivs; i++)
+            {
+                Handles.DrawLine(new Vector3(gridSpacing * i, -gridSpacing, 0) + newOffset, new Vector3(gridSpacing * i, position.height, 0f) + newOffset);
+            }
+
+            for (int j = 0; j < heightDivs; j++)
+            {
+                Handles.DrawLine(new Vector3(-gridSpacing, gridSpacing * j, 0) + newOffset, new Vector3(position.width, gridSpacing * j, 0f) + newOffset);
+            }
+
+            Handles.color = Color.white;
+            Handles.EndGUI();
+        }
+    }
+}
