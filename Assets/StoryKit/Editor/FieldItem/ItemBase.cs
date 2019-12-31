a0bc3385374159867ba333a3e17f5ccd3425df90
薛定谔的蟑螂 <xdedzl@163.com>
2019-12-31 17:35:05 +0800

    。

---
+++ Assets/StoryKit/Editor/FieldItem/ItemBase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UIElements;
+using System.Reflection;
+
+public class ItemBase : VisualElement
+{
+    private object m_Value;
+    public object Value
+    {
+        get
+        {
+            return m_Value;
+        }
+        set
+        {
+            m_Value = Value;
+        }
+    }
+}
+
+public class StringItem : VisualElement
+{
+    public StringItem(object target, FieldInfo info)
+    {
+        TextField textField = new TextField
+        {
+            label = info.Name,
+            value = info.GetValue(target) as string,
+        };
+        textField.RegisterValueChangedCallback((v) => 
+        {
+            info.SetValue(target, v.newValue);
+        });
+        textField.AddToClassList("item");
+        this.Add(textField);
+    }
+}
